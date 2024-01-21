654e04d0c2ff3933130be8d66fd19cc3fbfa89fb
prathamesh g <prathameshgirase03@gmail.com>
2024-01-22 00:56:10 +0530

    Code Fix

--- src/main/java/com/example/concertsystem/service/firebase/FirebaseServiceImpl.java
+++ src/main/java/com/example/concertsystem/service/firebase/FirebaseServiceImpl.java
@@ -1,88 +1,88 @@
-package com.example.concertsystem.service.firebase;
-
-import com.google.auth.Credentials;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.*;
-import com.google.firebase.cloud.StorageClient;
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.*;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.Objects;
-import java.util.UUID;
-
-@Service
-public class FirebaseServiceImpl implements FirebaseService {
-
-    private Storage storage;
-
-    public FirebaseServiceImpl() {
-
-        this.storage = StorageOptions.getDefaultInstance().getService();
-    }
-
-    @Override
-    public String upload(MultipartFile multipartFile) {
-        try {
-            String fileName = multipartFile.getOriginalFilename();                        // to get original file name
-            fileName = UUID.randomUUID().toString().concat(this.getExtension(fileName));  // to generated random string values for file name.
-
-            File file = this.convertToFile(multipartFile, fileName);                      // to convert multipartFile to File
-            String URL = this.uploadFile(file, fileName);                                   // to get uploaded file link
-            file.delete();
-            return URL;
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Image couldn't upload, Something went wrong";
-        }
-    }
-
-    @Override
-    public String download(String fileName) throws IOException {
-        String destFileName = UUID.randomUUID().toString().concat(this.getExtension(fileName));
-        String destFilePath = "D:\\" + destFileName;
-
-        InputStream inputStream = FirebaseService.class.getClassLoader().getResourceAsStream("concert-ticketing-system-67922-firebase-adminsdk-66i0l-ab05e09b6c.json");
-        Credentials credentials = GoogleCredentials.fromStream(inputStream);
-        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-        Blob blob = storage.get(BlobId.of("concert-ticketing-system-67922.appspot.com", fileName));
-        blob.downloadTo(Paths.get(destFilePath));
-
-        return "Image downloaded successfully : " + fileName;
-    }
-
-
-    private String uploadFile(File file, String fileName) throws IOException {
-        BlobId blobId = BlobId.of("concert-ticketing-system-67922.appspot.com", fileName); // Replace with your bucker name
-        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("media").build();
-        InputStream inputStream = FirebaseService.class.getClassLoader().getResourceAsStream("concert-ticketing-system-67922-firebase-adminsdk-66i0l-ab05e09b6c.json");
-        Credentials credentials = GoogleCredentials.fromStream(inputStream);
-        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-        storage.create(blobInfo, Files.readAllBytes(file.toPath()));
-
-        String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/concert-ticketing-system-67922.appspot.com/o";
-        return String.format(DOWNLOAD_URL, URLEncoder.encode(fileName, StandardCharsets.UTF_8));
-    }
-
-    private File convertToFile(MultipartFile multipartFile, String fileName) throws IOException {
-        File tempFile = new File(fileName);
-        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
-            fos.write(multipartFile.getBytes());
-            fos.close();
-        }
-        return tempFile;
-    }
-
-    private String getExtension(String fileName) {
-        return fileName.substring(fileName.lastIndexOf("."));
-    }
-
-
-}
-
+//package com.example.concertsystem.service.firebase;
+//
+//import com.google.auth.Credentials;
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.cloud.storage.*;
+//import com.google.firebase.cloud.StorageClient;
+//import org.springframework.core.io.ByteArrayResource;
+//import org.springframework.core.io.Resource;
+//import org.springframework.stereotype.Service;
+//import org.springframework.web.multipart.MultipartFile;
+//
+//import java.io.*;
+//import java.net.URLEncoder;
+//import java.nio.charset.StandardCharsets;
+//import java.nio.file.Files;
+//import java.nio.file.Paths;
+//import java.util.Objects;
+//import java.util.UUID;
+//
+//@Service
+//public class FirebaseServiceImpl implements FirebaseService {
+//
+//    private Storage storage;
+//
+//    public FirebaseServiceImpl() {
+//
+//        this.storage = StorageOptions.getDefaultInstance().getService();
+//    }
+//
+//    @Override
+//    public String upload(MultipartFile multipartFile) {
+//        try {
+//            String fileName = multipartFile.getOriginalFilename();                        // to get original file name
+//            fileName = UUID.randomUUID().toString().concat(this.getExtension(fileName));  // to generated random string values for file name.
+//
+//            File file = this.convertToFile(multipartFile, fileName);                      // to convert multipartFile to File
+//            String URL = this.uploadFile(file, fileName);                                   // to get uploaded file link
+//            file.delete();
+//            return URL;
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            return "Image couldn't upload, Something went wrong";
+//        }
+//    }
+//
+//    @Override
+//    public String download(String fileName) throws IOException {
+//        String destFileName = UUID.randomUUID().toString().concat(this.getExtension(fileName));
+//        String destFilePath = "D:\\" + destFileName;
+//
+//        InputStream inputStream = FirebaseService.class.getClassLoader().getResourceAsStream("concert-ticketing-system-67922-firebase-adminsdk-66i0l-ab05e09b6c.json");
+//        Credentials credentials = GoogleCredentials.fromStream(inputStream);
+//        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+//        Blob blob = storage.get(BlobId.of("concert-ticketing-system-67922.appspot.com", fileName));
+//        blob.downloadTo(Paths.get(destFilePath));
+//
+//        return "Image downloaded successfully : " + fileName;
+//    }
+//
+//
+//    private String uploadFile(File file, String fileName) throws IOException {
+//        BlobId blobId = BlobId.of("concert-ticketing-system-67922.appspot.com", fileName); // Replace with your bucker name
+//        BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("media").build();
+//        InputStream inputStream = FirebaseService.class.getClassLoader().getResourceAsStream("concert-ticketing-system-67922-firebase-adminsdk-66i0l-ab05e09b6c.json");
+//        Credentials credentials = GoogleCredentials.fromStream(inputStream);
+//        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+//        storage.create(blobInfo, Files.readAllBytes(file.toPath()));
+//
+//        String DOWNLOAD_URL = "https://firebasestorage.googleapis.com/v0/b/concert-ticketing-system-67922.appspot.com/o";
+//        return String.format(DOWNLOAD_URL, URLEncoder.encode(fileName, StandardCharsets.UTF_8));
+//    }
+//
+//    private File convertToFile(MultipartFile multipartFile, String fileName) throws IOException {
+//        File tempFile = new File(fileName);
+//        try (FileOutputStream fos = new FileOutputStream(tempFile)) {
+//            fos.write(multipartFile.getBytes());
+//            fos.close();
+//        }
+//        return tempFile;
+//    }
+//
+//    private String getExtension(String fileName) {
+//        return fileName.substring(fileName.lastIndexOf("."));
+//    }
+//
+//
+//}
+//
