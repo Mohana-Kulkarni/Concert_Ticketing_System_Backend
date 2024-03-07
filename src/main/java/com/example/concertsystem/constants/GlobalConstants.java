fa38c470c4de832540e28c0a073f90224dd591f2
Mohana-Kulkarni <mpkulkarni117@gmail.com>
2024-03-07 11:49:48 +0530

    Global Constants code modified

--- src/main/java/com/example/concertsystem/constants/GlobalConstants.java
+++ src/main/java/com/example/concertsystem/constants/GlobalConstants.java
@@ -9,6 +9,7 @@ public final class GlobalConstants {
 
     public static final String  STATUS_201 = "201";
     public static final String STATUS_409 = "409";
+    public static final String STATUS_451 = "451";
     public static final String  MESSAGE_201_Event = "Event Created successfully";
     public static final String  MESSAGE_201_User = "User Created successfully";
     public static final String  MESSAGE_201_Venue = "Venue Created successfully";
@@ -23,5 +24,7 @@ public final class GlobalConstants {
     public static final String  MESSAGE_417_DELETE= "Delete operation failed. Please try again or contact Dev team";
     public static final String  STATUS_500 = "500";
     public static final String  MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
+    public static final String  MESSAGE_451 = "The resource has never existed";
+
 
 }
