397c6e51d55e1d18012084bd57fa8ccfa48067f1
prathamesh g <prathameshgirase03@gmail.com>
2024-03-12 00:10:43 +0530

    Mail Service Added

---
+++ src/main/java/com/example/concertsystem/entity/Mail.java
@@ -0,0 +1,26 @@
+package com.example.concertsystem.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+public class Mail {
+    private String subject;
+    private String message;
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
